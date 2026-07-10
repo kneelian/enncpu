cf1b9748d1fc7aa260396a97907fa1da21d00c1d
kneelian <3547240@gmail.com>
2026-07-10 04:05:52 +0200

    simple stub example intended to eventually supply a barebones int48 library

---
+++ examples/src/int48.asm
@@ -0,0 +1,53 @@
+.ORG 0x0000
+.SEC %PGA
+
+	ADRL A, @STK
+	ADRM A, @STK
+	WSP  A
+
+	ADRL A, @PROG
+	ADRM A, @PROG
+	JMR  A
+
+	ERR
+
+; takes int48 in AB and CD
+; (big endian)
+; returns int48 in EF
+; (big endian)
+@ADC_I48
+
+	MOV  E, A
+	MOV  F, B
+
+	ADD  E, C
+	ADD  F, D
+
+	CGT   B, F
+	ADD.P E, #1
+
+	RET
+
+%PGA
+
+.ORG 0x0400
+.SEC %PGB
+	@PROG
+
+	MOVL B, #0xfe
+	MOVM B, #0xff
+	MOVH B, #0xff
+
+	MOV  A, #3
+
+	MOV  D, #16
+	MOV  C, #3
+
+	JLA  @ADC_I48
+
+	ERR
+
+%PGB
+
+.ORG 0x4000
+@STK
